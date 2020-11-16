b6ef04cbb0b4872d38b17e7e2337e86e8bab4e52
MrEAZL <51537507+MrEAZL@users.noreply.github.com>
2020-11-16 11:58:48 +0300

    Update what.php

--- what.php
+++ what.php
@@ -1,3 +1,13 @@
 <?php
-echo "e";
+$image = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRAOopDwAKdXQ2KNcyL2FIeG1FP0PLjRuK3JA&usqp=CAU";
+$colors = array("x"=>array(), "y"=>array());
+$width = imagesx($image);
+$height = imagesy($image);
+for($x = 0; $x < $width; $x++){
+   for($y = 0; $y < $height; $y++){
+      colors[0]->$x
+      colors[1]->$y
+   }
+}
+echo $colors;
 ?>
